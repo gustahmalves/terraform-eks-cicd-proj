930bfd460a565861599be9f3797f28780babd9be
Gustavo <gus.hmalves@gmail.com>
2026-04-05 19:41:51 -0300

    updating modules.tf

--- modules.tf
+++ modules.tf
@@ -12,6 +12,10 @@ module "eks-cluster" {
 }
 
 module "mng-node-group" {
-  source = "./modules/managed-node-group"
+  source           = "./modules/managed-node-group"
+  eks_cluster_name = module.eks-cluster.eks_cluster_name
+  subnet_1a        = module.network.subnet_1a
+  subnet_1b        = module.network.subnet_1b
+  subnet_1c        = module.network.subnet_1c
 
 }
--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "vpc_config" {
   value = module.eks-cluster.eks_vpc_config
 }
 
-output "oidc_identity" {
-  value = module.eks-cluster.oidc_identity
+output "oidc" {
+  value = module.eks-cluster.oidc
 
 }
